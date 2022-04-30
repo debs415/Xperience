d1cacc543642de6cf88f9f29265e9206a5acba90
Debanshi Todi <todidebanshi415@gmail.com>
2022-04-30 02:44:59 -0400

    header added (rough)

--- src/header/header1.jsx
+++ src/header/header1.jsx
@@ -1,10 +1,9 @@
-import styles from "./header.module.css";
 import Grid from "@material-ui/core/Grid";
 import { motion } from "framer-motion";
 
 export default function Header() {
 	return (
-		<header id={styles.header}>
+		<header>
 			<Grid
 				component={motion.div}
 				animate={{ rotate: 360 }}
@@ -13,28 +12,14 @@ export default function Header() {
 				className={styles.headerContainer}
 			>
 				{/* href to home page of Xperience */}
-				<a
-					className={styles.headerContainer}
-					href="/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<img
-						id={styles.headerImage}
-						src="/xperience.png"
-						alt="Xperience logo"
-					/>
+				<a href="/" target="_blank" rel="noreferrer">
+					<img src="/xperience.png" alt="Xperience logo" />
 				</a>
 			</Grid>
 			<div>
 				<motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
 					{/* href to about  */}
-					<a
-						className={styles.headerLink}
-						href="/"
-						target="_blank"
-						rel="noreferrer"
-					>
+					<a href="/" target="_blank" rel="noreferrer">
 						About
 					</a>
 				</motion.div>
@@ -42,12 +27,7 @@ export default function Header() {
 			<div>
 				<motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
 					{/* href to Contact Us  */}
-					<a
-						className={styles.headerLink}
-						href="/"
-						target="_blank"
-						rel="noreferrer"
-					>
+					<a href="/" target="_blank" rel="noreferrer">
 						Contact Us
 					</a>
 				</motion.div>
@@ -55,12 +35,7 @@ export default function Header() {
 			<div>
 				<motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
 					{/* href to login/register */}
-					<a
-						className={styles.headerLink}
-						href="/"
-						target="_blank"
-						rel="noreferrer"
-					>
+					<a href="/" target="_blank" rel="noreferrer">
 						Login/Register
 					</a>
 				</motion.div>
---
+++ src/header/header2.jsx
@@ -0,0 +1,102 @@
+import { ReactNode } from "react";
+import {
+	Box,
+	Flex,
+	Avatar,
+	Link,
+	Button,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+	useDisclosure,
+	useColorModeValue,
+	Stack,
+	useColorMode,
+	Center,
+	IconButton,
+	HStack,
+	Spacer,
+} from "@chakra-ui/react";
+import { Image } from "@chakra-ui/image";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+const Links = ["About", "Contact Us", "Login", "Sign Up"];
+
+const NavLink = ({ children }) => (
+	<Link
+		px={2}
+		py={1}
+		rounded={"md"}
+		_hover={{
+			textDecoration: "none",
+			bg: useColorModeValue("gray.200", "gray.700"),
+		}}
+		href={"#"}
+	>
+		{children}
+	</Link>
+);
+export default function Nav() {
+	const { colorMode, toggleColorMode } = useColorMode();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		<>
+			<Box
+				bg={useColorModeValue("gray.100", "gray.900")}
+				// color={useColorModeValue("yellow", "gray.800")}
+				px={4}
+			>
+				<HStack spacing={8}>
+					<Flex
+						w="100%"
+						h={16}
+						alignItems={"center"}
+						justifyContent={"space-between"}
+					>
+						{/* <Box> */}
+
+						<Image
+							boxSize="150px"
+							objectFit="contain"
+							src="/xperience.png"
+							alt="Xperience logo"
+						/>
+						{/* </Box> */}
+						<Spacer></Spacer>
+
+						<HStack
+							as={"nav"}
+							spacing={8}
+							display={{ base: "none", md: "flex" }}
+						>
+							{Links.map((link) => (
+								<NavLink key={link}>{link}</NavLink>
+							))}
+							{/* show loginmodal when clicked  */}
+							<Button
+								colorScheme="yellow"
+								variant="contain"
+
+								// border="2px"
+								// borderRadius=
+								// borderColor="black"
+							>
+								Login
+							</Button>
+						</HStack>
+
+						<Flex alignItems={"center"}>
+							<Stack direction={"row"}>
+								<Button onClick={toggleColorMode}>
+									{colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+								</Button>
+							</Stack>
+						</Flex>
+					</Flex>
+				</HStack>
+			</Box>
+		</>
+	);
+}
